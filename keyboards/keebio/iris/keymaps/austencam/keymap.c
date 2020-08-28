b0cdfb024b590bb1b807d419bbf4bd10af231828
Austen Cameron <austen.cameron@gmail.com>
2020-08-28 14:16:05 -0600

    added code related macros

--- keyboards/keebio/iris/keymaps/austencam/keymap.c
+++ keyboards/keebio/iris/keymaps/austencam/keymap.c
@@ -8,7 +8,6 @@ extern keymap_config_t keymap_config;
 #define _LOWER 1
 #define _RAISE 2
 #define _GAME 3
-#define _DVORAK 4
 #define _ADJUST 16
 
 enum custom_keycodes {
@@ -17,13 +16,17 @@ enum custom_keycodes {
   RAISE,
   ADJUST,
   EQL_ARROW,
-  DASH_ARROW
+  DASH_ARROW,
+  PHP_THIS,
+  CSS_CLASS,
+  PHP_FUNC,
+  JS_FUNC,
+  CODE_IF
 };
 
 // Shortcuts and some mod tap keys
 #define KC_ KC_TRNS
 #define KC_ESCC MT(MOD_LCTL, KC_ESC)
-#define KC_DVRK TG(_DVORAK)
 #define KC_LOWR LOWER
 #define KC_RASE RAISE
 #define KC_LBSP LT(_LOWER, KC_BSPC)
@@ -33,8 +36,13 @@ enum custom_keycodes {
 #define KC_DBG DEBUG
 #define KC_GSPC LGUI_T(KC_SPC)
 #define KC_GAME TG(_GAME)
-#define KC_EARR EQL_ARROW
+#define KC_EARR EQL_ARROW 
 #define KC_DARR DASH_ARROW
+#define KC_THIS PHP_THIS
+#define KC_CSSC CSS_CLASS
+#define KC_PHPF PHP_FUNC
+#define KC_JSFN JS_FUNC
+#define KC_CDIF CODE_IF
 
 // RGB Underglow and Key Backlight
 #define KC_RTOG RGB_TOG
@@ -63,21 +71,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //                  `----+----+----'        `----+----+----'
   ),
 
-  [_DVORAK] = LAYOUT_kc(
-  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-     GRV , 1  , 2  , 3  , 4  , 5  ,                6  , 7  , 8  , 9  , 0  ,MINS,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     TAB ,QUOT,COMM,DOT , P  , Y  ,                F  , G  , C  , R  , L  ,SLSH,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     ESCC, A  , O  , E  , U  , I  ,                D  , H  , T  , N  ,  S ,MINS,
-  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-     LSPO,SCLN, Q  , J  , K  , X  ,LGUI,     SPC , B  , M  ,  W  , V , Z  ,RSPC,
-  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                       LALT,    , LGUI,        SPC ,    ,LOWR 
-  //                  `----+----+----'        `----+----+----'
-  ),
-
-
   [_GAME] = LAYOUT_kc(
   // Same as QWERTY except these keym
   //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
@@ -97,11 +90,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
      TILD,EXLM, AT ,HASH,DLR ,PERC,               CIRC,AMPR,ASTR,LPRN,RPRN, EQL,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-         ,    ,    ,    ,    ,LPRN,               RPRN, P7 , P8 , P9 ,    ,VOLU,
+         ,    ,    ,JSFN,PHPF,LPRN,               RPRN, P7 , P8 , P9 ,    ,VOLU,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-         ,    ,    ,DARR,EARR,LBRC,               RBRC, P4 , P5 , P6 ,    ,VOLD,
+         ,CDIF,THIS,DARR,EARR,LBRC,               RBRC, P4 , P5 , P6 ,    ,VOLD,
   //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-         ,    ,    ,    ,    ,LCBR,    ,      SPC,RCBR, P1 , P2 , P3 , P0 ,MUTE,
+         ,    ,    ,CSSC,    ,LCBR,    ,      SPC,RCBR, P1 , P2 , P3 , P0 ,MUTE,
   //`----+----+----+----+----+----+----/    \----+----+----+----+----+----+----'
                            ,    ,    ,          SPC,RASE, 
   //                  `----+----+----'        `----+----+----'
@@ -113,7 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
          ,    ,    ,    ,    ,LPRN,               HOME,PGDN,PGUP, END,    , F12,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-         ,    ,    ,    ,    ,LBRC,               LEFT,DOWN, UP ,RGHT,    ,    ,
+         ,    ,    ,JSFN,PHPF,LBRC,               LEFT,DOWN, UP ,RGHT,    ,    ,
   //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
          ,    ,    ,    ,    ,LCBR,    ,     GSPC,    ,    ,    ,    ,     ,   ,
   //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
@@ -129,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
          ,    ,RHUD,RSAD,RVAD,    ,                   ,    ,    ,    ,    ,    ,
   //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-         ,    ,    ,    ,    ,    ,    ,         ,    ,    ,    ,    ,     ,DVRK,
+         ,    ,    ,    ,    ,    ,    ,         ,    ,    ,    ,    ,    ,    ,
   //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
                            ,    ,    ,             ,    , 
   //                  `----+----+----'        `----+----+----'  
@@ -174,6 +167,31 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case DASH_ARROW:
       if (record->event.pressed) {
         SEND_STRING("->");
+      }
+      break;
+    case PHP_THIS:
+      if (record->event.pressed) {
+        SEND_STRING("$this->");
+      }
+      break;
+    case CSS_CLASS:
+      if (record->event.pressed) {
+        SEND_STRING("class="SS_LSFT("'")SS_LSFT("'")SS_TAP(X_LEFT));
+      }
+      break;
+    case PHP_FUNC:
+      if (record->event.pressed) {
+        SEND_STRING("function() {}"SS_TAP(X_LEFT));
+      }
+      break;
+    case JS_FUNC:
+      if (record->event.pressed) {
+        SEND_STRING("() => {}"SS_TAP(X_LEFT));
+      }
+      break;
+    case CODE_IF:
+      if (record->event.pressed) {
+        SEND_STRING("if () {}"SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT));
       }
       break;
   }
